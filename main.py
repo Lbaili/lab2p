a524c88c2c87d2a61687367807f24e6b565674f8
Chang Li <1295118952@qq.com>
2020-09-08 14:27:37 +0000

    .

--- main.py
+++ main.py
@@ -4,36 +4,31 @@
 # Section: 1
 # Breakout: 14
 
-#include <stdio.h>
-#include <readline/readline.h>
-#include <stdlib.h>
-
-  char *a = readline("Enter your CMPSC 131 grade: ");
-  double g = atof(a);
+  g = input("Enter your CMPSC 131 grade: ")
   if g<60:
-    printf("Your letter grade for CMPSC 131 is F.\n");
+    print(f"Your letter grade for CMPSC 131 is F.\n")
   }
   elif g<70:
-    printf("Your letter grade for CMPSC 131 is D.\n");
+    print(f"Your letter grade for CMPSC 131 is D.\n")
   }
   elif g<77:
-    printf("Your letter grade for CMPSC 131 is C.\n");
+    print(f"Your letter grade for CMPSC 131 is C.\n")
   }
   elif g<80:
-    printf("Your letter grade for CMPSC 131 is C+.\n");
+    print(f"Your letter grade for CMPSC 131 is C+.\n")
   }
   elif g<83:
-    printf("Your letter grade for CMPSC 131 is B-.\n");
+    print(f"Your letter grade for CMPSC 131 is B-.\n")
   }
   elif g<87:
-    printf("Your letter grade for CMPSC 131 is B.\n");
+    print(f"Your letter grade for CMPSC 131 is B.\n")
   }
   elif g<90:
-    printf("Your letter grade for CMPSC 131 is B+.\n");
+    print(f"Your letter grade for CMPSC 131 is B+.\n")
   }
   elif g<93 :
-    printf("Your letter grade for CMPSC 131 is A-.\n");
+    print(f"Your letter grade for CMPSC 131 is A-.\n")
   
   else :
-    printf("Your letter grade for CMPSC 131 is A.\n");
+    print(f"Your letter grade for CMPSC 131 is A.\n")
   
